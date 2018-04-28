82962933c6dbbd61fb036ba8f09e174d018ecca6
228saravi <228saravi@gmail.com>
2018-04-28 15:45:22 +0300

    comit

---
+++ src/views/About/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const CustomSkinMap = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{ lat: 53.902429, lng: 30.338885 }}
+      defaultOptions={{
+        scrollwheel: false,
+        zoomControl: true,
+        
+      }}
+    >
+      <Marker position={{ lat: 53.902429, lng: 30.338885 }} />
+    </GoogleMap>
+  ))
+);
+
+function Maps({ ...props }) {
+  return (
+    <CustomSkinMap
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzaOm_m8RwN_dM7xzTx-HZAvLu0hnN_B4"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `100vh` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  );
+}
+
+export default Maps;
